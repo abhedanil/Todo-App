143e9ca2c3868d6f40cda64b67872131a796eea3
Abhed Anil <abhedanil@gmail.com>
2022-08-10 21:24:19 +0530

    todo first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState} from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+ 
+const [todo,setTodo] =useState("")
+const [todos,setTodos] =useState([])
+const [editId, setEditId] = useState(0);
+
+const handleSubmit=(e)=>{
+ e.preventDefault();
+
+if(editId){
+  const editTodo = todos.find((i)=> i.id===editId)
+  const updatedTodos = todos.map((t)=>
+  t.id=== editTodo.id ?
+  (t= {id: t.id,todo})
+  :{id: t.id, todo : t.todo}
+  )
+  setTodos(updatedTodos)
+  setEditId(0)
+  setTodo("")
+  return;
 }
 
-export default App;
+ if(todo!=='')
+ {
+  setTodos([{id:`${todo}-${Date.now()}` ,todo}, ...todos])
+  setTodo("")
+ }
+};
+
+const handleDelete=(id)=>{
+  const delTodo = todos.filter((to)=> to.id !== id)
+  setTodos([...delTodo])
+}
+
+const handleEdit= (id)=>{
+  const editTodo = todos.find((i)=>i.id===id)
+  setTodo(editTodo.todo);
+  setEditId(id);
+}
+
+  return( 
+    <div className='App'>
+      <div className='container' >
+
+        <h1>Todo List App</h1>
+        <form className='todo-form' onSubmit={handleSubmit} >
+        <input className='inpufieldtext' placeholder="Add task..." type="text" value={todo} onChange={(e)=>{
+          setTodo(e.target.value)
+        }}/>
+        <button className='addbutton' type="submit"> { editId ? "Edit" : <i class="fa-solid fa-plus"></i>}</button>
+        </form>
+        <ul className='allTodos'>
+          {
+            todos.map((t)=>(
+
+              <li className='singleTodo'>
+                <span className='todoText' key={t.id}>{t.todo}</span>
+                <button className='btncolor' onClick={()=>handleEdit(t.id)}><i class="fa-solid fa-file-pen"></i></button>
+                <button className='btncolor' onClick={()=> handleDelete(t.id)} ><i class="fa-solid fa-trash-can"></i></button>
+              </li>
+            ))
+          }
+
+
+        </ul>
+      </div>
+      
+    </div>
+   
+  )
+}
+
+export default App
